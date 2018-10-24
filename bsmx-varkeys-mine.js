62ab3eb3b51df55f10543e06b84cc77d4be0c054
Matt Kulka <mattk@cadth.ca>
2018-10-24 19:46:34 -0400

    Update 10.24 - still no success
    After a day smashing the keys - still no success with rendering the recpies.  Will try again

--- bsmx-varkeys-mine.js
+++ bsmx-varkeys-mine.js
@@ -1,8 +1,8 @@
 var xml_urls = {
-  'recipes' : 'http://www.kulka.ca/beers/bsmx\Recipe.bsmx',
-  'hops': 'http://www.kulka.ca/beers/bsmx\Hops.bsmx',
-  'grains' : 'http://www.kulka.ca/beers/bsmx\Grain.bsmx',
-  'yeasts' : 'http://www.kulka.ca/beers/bsmx\Yeast.bsmx'
+  'recipes' : 'https://drive.google.com/open?id=1upGHcPcnzpZrhu_nQ37f_kBz9xpK2kzU',
+  'hops': 'https://drive.google.com/open?id=1trm3iVjK3kFj7Q8KUFp_7JWWeH5lnp0O',
+  'grains' : 'https://drive.google.com/open?id=1ItAsHdNpG39N-dvKQ5yTl3DSvC6N0fyI',
+  'yeasts' : 'https://drive.google.com/open?id=14-yvqGh6bCi3Me89A8Yg6oyfA4tgqI5_'
 }
 
 var page_urls = {
